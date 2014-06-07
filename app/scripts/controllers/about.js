989b176907e0a4636cacbb254c8325f005a5b2c0
Dane Stuckel <dane.stuckel@gmail.com>
2014-06-06 21:21:22 -0600

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name retconWebsiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the retconWebsiteApp
+ */
+angular.module('retconWebsiteApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name retconWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the retconWebsiteApp
+ */
+angular.module('retconWebsiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
